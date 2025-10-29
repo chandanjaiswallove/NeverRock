4f432327de46cf102a99b614a3253a06cc3ea1f6
chandanjaiswallove <chandanjaiswalloves@gmail.com>
2025-10-29 13:57:07 +0530

    Add stuent menu in tabs

--- application/views/dashboard/master_contents/dStudent_master/student_header.php
+++ application/views/dashboard/master_contents/dStudent_master/student_header.php
@@ -264,7 +264,7 @@ if (!$this->session->userdata('activeStudent')) {
                                                                 points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2">
                                                             </polygon>
                                                         </svg>
-                                                        Reviews</a>
+                                                        Downloads</a>
                                                 </li>
                                                 <li
                                                     class="py-10px border-b border-borderColor dark:border-borderColor-dark">
@@ -281,8 +281,93 @@ if (!$this->session->userdata('activeStudent')) {
                                                             <line x1="12" y1="17" x2="12.01" y2="17">
                                                             </line>
                                                         </svg>
-                                                        Quiz
-                                                        Attempts</a>
+                                                        Student Card
+                                                        </a>
+                                                </li>
+                                                <li
+                                                    class="py-10px border-b border-borderColor dark:border-borderColor-dark">
+                                                    <a href="<?php echo base_url('student_setting'); ?>"
+                                                        class="text-contentColor dark:text-contentColor-dark hover:text-primaryColor dark:hover:text-primaryColor leading-1.8 flex gap-3 text-nowrap"><svg
+                                                            xmlns="http://www.w3.org/2000/svg" width="16" height="24"
+                                                            viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                            stroke-width="2" stroke-linecap="round"
+                                                            stroke-linejoin="round" class="feather feather-star">
+                                                            <polygon
+                                                                points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2">
+                                                            </polygon>
+                                                        </svg>
+                                                        Certificate</a>
+                                                </li>
+
+                                                 <li
+                                                    class="py-10px border-b border-borderColor dark:border-borderColor-dark">
+                                                    <a href="<?php echo base_url('student_setting'); ?>"
+                                                        class="text-contentColor dark:text-contentColor-dark hover:text-primaryColor dark:hover:text-primaryColor leading-1.8 flex gap-3 text-nowrap"><svg
+                                                            xmlns="http://www.w3.org/2000/svg" width="16" height="24"
+                                                            viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                            stroke-width="2" stroke-linecap="round"
+                                                            stroke-linejoin="round" class="feather feather-star">
+                                                            <polygon
+                                                                points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2">
+                                                            </polygon>
+                                                        </svg>
+                                                        Invoice</a>
+                                                </li>
+                                               <li
+                                                    class="py-10px border-b border-borderColor dark:border-borderColor-dark">
+                                                    <a href="<?php echo base_url('student_setting'); ?>"
+                                                        class="text-contentColor dark:text-contentColor-dark hover:text-primaryColor dark:hover:text-primaryColor leading-1.8 flex gap-3 text-nowrap"><svg
+                                                            xmlns="http://www.w3.org/2000/svg" width="16" height="24"
+                                                            viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                            stroke-width="2" stroke-linecap="round"
+                                                            stroke-linejoin="round" class="feather feather-star">
+                                                            <polygon
+                                                                points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2">
+                                                            </polygon>
+                                                        </svg>
+                                                        Live Classes</a>
+                                                </li>
+                                                <li
+                                                    class="py-10px border-b border-borderColor dark:border-borderColor-dark">
+                                                    <a href="<?php echo base_url('student_setting'); ?>"
+                                                        class="text-contentColor dark:text-contentColor-dark hover:text-primaryColor dark:hover:text-primaryColor leading-1.8 flex gap-3 text-nowrap"><svg
+                                                            xmlns="http://www.w3.org/2000/svg" width="16" height="24"
+                                                            viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                            stroke-width="2" stroke-linecap="round"
+                                                            stroke-linejoin="round" class="feather feather-star">
+                                                            <polygon
+                                                                points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2">
+                                                            </polygon>
+                                                        </svg>
+                                                        Doubt</a>
+                                                </li>
+                                             <li
+                                                    class="py-10px border-b border-borderColor dark:border-borderColor-dark">
+                                                    <a href="<?php echo base_url('student_setting'); ?>"
+                                                        class="text-contentColor dark:text-contentColor-dark hover:text-primaryColor dark:hover:text-primaryColor leading-1.8 flex gap-3 text-nowrap"><svg
+                                                            xmlns="http://www.w3.org/2000/svg" width="16" height="24"
+                                                            viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                            stroke-width="2" stroke-linecap="round"
+                                                            stroke-linejoin="round" class="feather feather-star">
+                                                            <polygon
+                                                                points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2">
+                                                            </polygon>
+                                                        </svg>
+                                                        Routine</a>
+                                                </li>
+                                                <li
+                                                    class="py-10px border-b border-borderColor dark:border-borderColor-dark">
+                                                    <a href="<?php echo base_url('student_setting'); ?>"
+                                                        class="text-contentColor dark:text-contentColor-dark hover:text-primaryColor dark:hover:text-primaryColor leading-1.8 flex gap-3 text-nowrap"><svg
+                                                            xmlns="http://www.w3.org/2000/svg" width="16" height="24"
+                                                            viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                            stroke-width="2" stroke-linecap="round"
+                                                            stroke-linejoin="round" class="feather feather-star">
+                                                            <polygon
+                                                                points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2">
+                                                            </polygon>
+                                                        </svg>
+                                                        Tests</a>
                                                 </li>
                                             </ul>
                                             <!-- user -->
