e800cdedd505a2c5b7b79a7641a04f70ef8425b8
chandanjaiswallove <chandanjaiswalloves@gmail.com>
2025-11-19 22:24:24 +0530

    update profile ui

--- application/views/dashboard/dAdmin/admin_setting.php
+++ application/views/dashboard/dAdmin/admin_setting.php
@@ -45,59 +45,112 @@ $this->load->view('dashboard/master_contents/dAdmin_master/admin_header');
                         </button>
                     </div>
                     <div class="tab-contents">
+                        <!-- Profile Section Here -->
                         <div class="transition-all duration-300">
-                            <form class="text-sm text-blackColor dark:text-blackColor-dark leading-1.8"
+                            <form action="" method="" class="text-sm text-blackColor dark:text-blackColor-dark leading-1.8"
                                 data-aos="fade-up">
                                 <div class="grid grid-cols-1 xl:grid-cols-2 mb-15px gap-y-15px gap-x-30px">
+
                                     <div>
-                                        <label class="mb-3 block font-semibold">First Name</label>
-                                        <input type="text" placeholder="John"
+                                        <label for="profilePhoto" class="mb-3 block font-semibold">Profile Photo</label>
+                                        <input type="file" id="profilePhoto" name="profilePhoto"
                                             class="w-full py-10px px-5 text-sm focus:outline-none text-contentColor dark:text-contentColor-dark bg-whiteColor dark:bg-whiteColor-dark border-2 border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 leading-23px rounded-md font-no">
                                     </div>
+
                                     <div>
-                                        <label class="mb-3 block font-semibold">Last Name</label>
-                                        <input type="text" placeholder="Due"
+                                        <label for="expertAs" class="mb-3 block font-semibold">Expert As</label>
+                                        <input type="text" id="expertAs" name="expertAs" placeholder="teacher"
                                             class="w-full py-10px px-5 text-sm focus:outline-none text-contentColor dark:text-contentColor-dark bg-whiteColor dark:bg-whiteColor-dark border-2 border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 leading-23px rounded-md font-no">
                                     </div>
+
                                     <div>
-                                        <label class="mb-3 block font-semibold">User Name</label>
-                                        <input type="text" placeholder="Ntaden Mic"
+                                        <label for="facebook" class="mb-3 block font-semibold">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="24"
+                                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                                stroke-linecap="round" stroke-linejoin="round"
+                                                class="feather feather-facebook inline-block mr-1">
+                                                <path
+                                                    d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z">
+                                                </path>
+                                            </svg>Facebook
+                                        </label>
+                                        <input type="text" id="facebook" name="facebook"
+                                            placeholder="https://facebook.com/"
                                             class="w-full py-10px px-5 text-sm focus:outline-none text-contentColor dark:text-contentColor-dark bg-whiteColor dark:bg-whiteColor-dark border-2 border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 leading-23px rounded-md font-no">
                                     </div>
+
                                     <div>
-                                        <label class="mb-3 block font-semibold">Phone Number</label>
-                                        <input type="text" placeholder="+1-202-555-0174"
+                                        <label for="twitter" class="mb-3 block font-semibold">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="24"
+                                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                                stroke-linecap="round" stroke-linejoin="round"
+                                                class="feather feather-twitter inline-block mr-1">
+                                                <path
+                                                    d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z">
+                                                </path>
+                                            </svg>Twitter
+                                        </label>
+                                        <input type="text" id="twitter" name="twitter"
+                                            placeholder="https://twitter.com/"
                                             class="w-full py-10px px-5 text-sm focus:outline-none text-contentColor dark:text-contentColor-dark bg-whiteColor dark:bg-whiteColor-dark border-2 border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 leading-23px rounded-md font-no">
                                     </div>
+
                                     <div>
-                                        <label class="mb-3 block font-semibold">Skill/Occupation</label>
-                                        <input type="text" placeholder="Full Stack Developer"
+                                        <label for="linkedin" class="mb-3 block font-semibold">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="24"
+                                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                                stroke-linecap="round" stroke-linejoin="round"
+                                                class="feather feather-linkedin inline-block mr-1">
+                                                <path
+                                                    d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z">
+                                                </path>
+                                                <rect x="2" y="9" width="4" height="12"></rect>
+                                                <circle cx="4" cy="4" r="2"></circle>
+                                            </svg>Linkedin
+                                        </label>
+                                        <input type="text" id="linkedin" name="linkedin"
+                                            placeholder="https://linkedin.com/"
                                             class="w-full py-10px px-5 text-sm focus:outline-none text-contentColor dark:text-contentColor-dark bg-whiteColor dark:bg-whiteColor-dark border-2 border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 leading-23px rounded-md font-no">
                                     </div>
+
                                     <div>
-                                        <label class="mb-3 block font-semibold">Display Name Publicly As</label>
-                                        <input type="text" placeholder="John"
+                                        <label for="github" class="mb-3 block font-semibold">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="24"
+                                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                                stroke-linecap="round" stroke-linejoin="round"
+                                                class="feather feather-github inline-block mr-1">
+                                                <path
+                                                    d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
+                                                </path>
+                                            </svg>Github
+                                        </label>
+                                        <input type="text" id="github" name="github" placeholder="https://github.com/"
                                             class="w-full py-10px px-5 text-sm focus:outline-none text-contentColor dark:text-contentColor-dark bg-whiteColor dark:bg-whiteColor-dark border-2 border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 leading-23px rounded-md font-no">
                                     </div>
-                                </div>
-                                <div class="mb-15px">
-                                    <label class="mb-3 block font-semibold">Bio</label>
-                                    <textarea
-                                        class="w-full py-10px px-5 text-sm text-contentColor dark:text-contentColor-dark bg-whiteColor dark:bg-whiteColor-dark border-2 border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 leading-23px rounded-md"
-                                        cols="30" rows="10">
-Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                     </textarea>
+
                                 </div>
 
+                                <div class="mb-15px">
+                                    <label for="biography" class="mb-3 block font-semibold">Biography</label>
+                                    <textarea id="biography" name="biography"
+                                        class="w-full py-10px px-5 text-sm text-contentColor dark:text-contentColor-dark bg-whiteColor dark:bg-whiteColor-dark border-2 border-borderColor dark:border-borderColor-dark placeholder:text-placeholder placeholder:opacity-80 leading-23px rounded-md resize-y"
+                                        style="max-height: 150px;" cols="30"
+                                        rows="5">Write Something about yourself..</textarea>
+                                </div>
+
+
                                 <div class="mt-15px">
-                                    <button type="submit"
+                                    <button type="submit" name="updateProfile"
                                         class="text-size-15 text-whiteColor bg-primaryColor px-25px py-10px border border-primaryColor hover:text-primaryColor hover:bg-whiteColor inline-block rounded group dark:hover:text-whiteColor dark:hover:bg-whiteColor-dark">
-                                        Update Info
+                                        Update Profile
                                     </button>
                                 </div>
                             </form>
+
+
                         </div>
 
+                        <!-- Password Section Here -->
                         <div class="hidden transition-all duration-300">
 
                             <form method="POST" action="<?php echo base_url('passwordUpdate'); ?>"
@@ -152,6 +205,7 @@ Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                             </form>
                         </div>
 
+                        <!-- Social LInks Section Here -->
                         <div class="hidden transition-all duration-300">
                             <form class="text-sm text-blackColor dark:text-blackColor-dark leading-1.8"
                                 data-aos="fade-up">
