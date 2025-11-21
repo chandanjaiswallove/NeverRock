f920f59adceb48a5ab2a66119b8602561f178ec6
chandanjaiswallove <chandanjaiswalloves@gmail.com>
2025-11-21 11:05:07 +0530

    admin dashboard

--- application/views/dashboard/dAdmin/admin_coursework.php
+++ application/views/dashboard/dAdmin/admin_coursework.php
@@ -93,14 +93,43 @@ $this->load->view('dashboard/master_contents/dAdmin_master/admin_header');
                                                                         <li>
                                                                             <a href="<?php echo base_url('admin_courseedit?id=' . $row->id); ?>"
                                                                                 class="text-sm font-semibold text-black border-l-2 border-transparent transition duration-300 hover:border-primaryColor px-25px py-10px hover:bg-whitegrey1 block hover:text-primaryColor dark:text-contentColor-dark dark:hover:text-primaryColor dark:hover:bg-whitegrey1-dark">
-                                                                                Edit
+                                                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                                                    width="14" height="24"
+                                                                                    viewBox="0 0 24 24" fill="none"
+                                                                                    stroke="currentColor" stroke-width="2"
+                                                                                    stroke-linecap="round"
+                                                                                    stroke-linejoin="round"
+                                                                                    class="feather feather-edit">
+                                                                                    <path
+                                                                                        d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7">
+                                                                                    </path>
+                                                                                    <path
+                                                                                        d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z">
+                                                                                    </path>
+                                                                                </svg> Edit
                                                                             </a>
                                                                         </li>
 
                                                                         <li>
                                                                             <a href="<?php echo base_url('admin_courseedit?id=' . $row->id); ?>"
                                                                                 class="text-sm font-semibold text-black border-l-2 border-transparent transition duration-300 hover:border-primaryColor px-25px py-10px hover:bg-whitegrey1 block hover:text-primaryColor dark:text-contentColor-dark dark:hover:text-primaryColor dark:hover:bg-whitegrey1-dark">
-                                                                                Delete
+                                                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                                                    width="14" height="24"
+                                                                                    viewBox="0 0 24 24" fill="none"
+                                                                                    stroke="currentColor" stroke-width="2"
+                                                                                    stroke-linecap="round"
+                                                                                    stroke-linejoin="round"
+                                                                                    class="feather feather-trash-2">
+                                                                                    <polyline points="3 6 5 6 21 6">
+                                                                                    </polyline>
+                                                                                    <path
+                                                                                        d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
+                                                                                    </path>
+                                                                                    <line x1="10" y1="11" x2="10" y2="17">
+                                                                                    </line>
+                                                                                    <line x1="14" y1="11" x2="14" y2="17">
+                                                                                    </line>
+                                                                                </svg> Delete
                                                                             </a>
                                                                         </li>
 
--- application/views/dashboard/dAdmin/admin_createInstructors.php
+++ application/views/dashboard/dAdmin/admin_createInstructors.php
@@ -20,13 +20,13 @@ $this->load->view('dashboard/master_contents/dAdmin_master/admin_header');
             <div
                 class="p-10px md:px-10 md:py-50px mb-30px bg-whiteColor dark:bg-whiteColor-dark shadow-accordion dark:shadow-accordion-dark rounded-standard">
                 <!-- heading -->
-                <div class="mb-6 pb-5 border-b-2 border-borderColor dark:border-borderColor-dark">
+                <div class="mb-6 pb-5 border-b-2 border-borderColor dark:border-borderColor-dark" data-aos="fade-up">
                     <h2 class="text-2xl font-bold text-blackColor dark:text-blackColor-dark">
                         Create an Instructor
                     </h2>
                 </div>
                 <div class="tab">
-                    <div class="tab-links flex flex-wrap mb-10px lg:mb-50px rounded gap-10px">
+                    <div class="tab-links flex flex-wrap mb-10px lg:mb-50px rounded gap-10px" data-aos="fade-up">
                         <button
                             class="is-checked relative py-10px px-5 md:py-15px lg:px-10 font-bold uppercase text-sm lg:text-base text-blackColor bg-whiteColor shadow-overview-button dark:bg-whiteColor-dark dark:text-blackColor-dark before:w-0 before:h-0.5 before:absolute before:-bottom-0.5 lg:before:bottom-0 before:left-0 before:bg-primaryColor hover:before:w-full before:transition-all before:duration-300 whitespace-nowrap active">
                             INSTRUCTOR DETAILS
